531f852acbb1a1944e402d970001f8a6e40453cf
josebenjamin04 <67609517+josebenjamin04@users.noreply.github.com>
2021-07-28 21:42:42 -0600

    index final

--- index.php
+++ index.php
@@ -8,11 +8,16 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body onload="Iniciar()">
-    <img class="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg" src="recursos_login/img/fotoColegio.jpg">
-    <h1 class="pt-4 text-2xl">Sistema de calificación</h1>
+    
+    
 	    <!-- Formulario -->
-        <section class="ml-20">
-            
+        
+        <section class="">
+        <article class="ml-10" >
+        <img class="m-auto  mr-2 w-9/12 hidden lg:block  rounded-l-lg" src="recursos_login/img/fotoColegio.jpg">
+        </article>
+       <article class="ml-4 -mt-96">
+       <h1 class=" text-2xl ml-4 ">Sistema de calificación</h1>
             <form class="px-8 pt-6 pb-8 mb-4 bg-white rounded" action="index.php" method="POST" id="formLogin">
                 <div class="grupo01" id="txtNombre">
                     <label class="block mb-2 text-sm font-bold text-gray-700" for="text01"><span>Usuario:</span></label>
@@ -40,18 +45,16 @@
                     <a href="contrasenaOlvidada.php" class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800">He olvidado mi contraseña...</a>
                 </div>
                 
-				<div class="" id="btnEnviar">
+				<div class="ml-16" id="btnEnviar">
                     <button type="submit" name="enviar" class="mr-20 mt-5 px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline  ">Iniciar</button>
                 </div>
 
-                <div id="btnAyuda">
-                    <button href="ayuda.html" class="ml-20  px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline  ">Ayuda</button>
-                </div>
+              
             </form>
-            
+       </article> 
         </section>
     <!-- Pie de página -->
-        <footer class="text-center mb-0">- Copyright Colegio Don Bosco 2020 -</footer>
+        <footer class="text-center mt-20 text-white p-10 mb-0 bg-black p">- Copyright Colegio Don Bosco 2020 -</footer>
     </div>
 </body>
 </html>
