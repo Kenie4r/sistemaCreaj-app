4152409b5e2cd6308632173df013651de8bc6d5c
jesusmejia04 <79819116+jesusmejia04@users.noreply.github.com>
2021-07-29 23:26:30 -0600

    por fin xd

--- Dashboard/Dashboard.php
+++ Dashboard/Dashboard.php
@@ -6,7 +6,8 @@
               <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <?php 
-                session_start();
+                require_once("../controlador/login.php");
+                entrar();
                 ?>    
                 <!--
                   Icon when menu is closed.
@@ -75,9 +76,9 @@
                   case 'j':
 
                   ?>
-                  <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Ver rubrica</a>
+                  <a href="../rubrics.j/index.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Ver rubrica</a>
 
-                  <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calificar</a>
+                  <a href="../calificar/index.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calificar</a>
                   <?php
                   break;
                   ?>
@@ -93,7 +94,7 @@
                 <div>
                   <button type="button" onclick="mostrarpremio();" class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                     <span class="sr-only">Open user menu</span>
-                    <img class="h-8 w-8 rounded-full" src="img/perfil.jpg" alt="">
+                    <img class="h-8 w-8 rounded-full" src="../recursos/perfil.jpg" alt="">
                   </button>
                 </div>
       
@@ -109,7 +110,7 @@
                 -->
                 <div id="premio" class="hidden hover:block origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                   <!-- Active: "bg-gray-100", Not Active: "" -->
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Editar perfil</a>
+                  <a href="../users_profile/index.php" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Editar perfil</a>
                   <a class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" href="../controlador/logout.php" id="user-menu-item-1">Salir</a>
 
                 </div>
