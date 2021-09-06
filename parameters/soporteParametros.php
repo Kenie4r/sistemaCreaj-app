9f8d921a58466187028fe28e2d9922c4c9deb38c
jesusmejia04 <79819116+jesusmejia04@users.noreply.github.com>
2021-09-06 09:42:44 -0600

    ultima cosa de parametros

--- parameters/soporteParametros.php
+++ parameters/soporteParametros.php
@@ -7,7 +7,7 @@ function comparacionFecha($nombre){
 	$DatosParametros= $consulta->getFechas($nombre);
 	date_default_timezone_set("America/El_Salvador");
 	$fecha = strtotime(date("Y-m-d", time()));
-	if (!($fecha >= strtotime($DatosParametros['paramFecha']) && $fecha < strtotime($DatosParametros['paramFechaF']))){
+	if (!($fecha >= strtotime($DatosParametros['paramFecha']) && $fecha <= strtotime($DatosParametros['paramFechaF']))){
 		?>
 		<div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
 			<div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
