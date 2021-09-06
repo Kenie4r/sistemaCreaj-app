1c9137d6769c706e050919ab74e7658c307508a5
Kenie4r <54483710+Kenie4r@users.noreply.github.com>
2021-09-06 16:09:42 -0600

    Update index.php

--- calificar/index.php
+++ calificar/index.php
@@ -3,7 +3,8 @@
     require('../modelo/conection.php');
     require('../modelo/query.php');
     require('libs/libs.php');
-    
+    require_once("../parameters/soporteParametros.php");
+    comparacionFecha("Limite de calificacion");
     //vamos a crear un nuevo html usando la clase creada para esto, llamada html
     $htmlCreator= new html();
     $htmlCreator->watchProjects( );
