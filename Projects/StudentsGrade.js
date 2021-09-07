50271068d364e1fd6fe3338172349de0f8da00fc
jesusmejia04 <79819116+jesusmejia04@users.noreply.github.com>
2021-09-07 12:15:55 -0600

    a

--- Projects/StudentsGrade.js
+++ Projects/StudentsGrade.js
@@ -10,7 +10,7 @@ function recargarlista(){
     $.ajax({
         type:"POST",
         url:"soportePorjectStudens.php",
-        data:"grados=" + $('txtGrado').val(),
+        data:"grados=" + $('#txtGrado').val(),
         success:function(r){
             $('#txtAlumnos').html(r);
         }
