f7857822b63b5c54216d507fda1e1d879f9068bf
Diego Mancía <62128556+DiegoSan03@users.noreply.github.com>
2021-09-08 11:00:44 -0600

    Boton con js

---
+++ js/script-newStudents.js
@@ -0,0 +1,12 @@
+$(document).ready(
+    function(){
+        //Se configura la validación del form al enviarlo
+        $("#btnSubmit").on("click",
+            function(){
+                var frmStudents = $("#frmNewRubric");
+                frmStudents.submit();
+                alert("hola");
+            }
+        )
+    }
+)
