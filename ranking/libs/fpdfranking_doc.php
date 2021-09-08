f55120083e2bc75a85f551373b6bbb817e041fe1
Kenie4r <54483710+Kenie4r@users.noreply.github.com>
2021-09-08 17:45:05 -0600

    Update fpdfranking_doc.php

--- ranking/libs/fpdfranking_doc.php
+++ ranking/libs/fpdfranking_doc.php
@@ -3,9 +3,10 @@
     require('../../modelo/conection.php');
     require('../../modelo/query.php');
 
+//instancia de query
+$query = new Query();
 
     //creación del pdf de el ranking de un grado y materia
-
 //id ge materia 
 $idGrado = $_GET['idGrado'];
 $idMateria = $_GET['idMateria'];
@@ -21,13 +22,23 @@ $idMateria = $_GET['idMateria'];
     $fpdf->AddPage('landscape', 'letter');
     //creación del documento 
     $fpdf->Cell(30, 10, "Puesto",1,0, 'c');
-    $fpdf->Cell(75, 10, "Nombre del proyecto", 1,0, 'c');
+    $fpdf->Cell(85, 10, "Nombre del proyecto", 1,0, 'c');
     $fpdf->Cell(75, 10 , "Puntos obtenidos", 1,0, 'c');
     $fpdf->Cell(30, 10 , "Grado", 1,0, 'c');
     $fpdf->Cell(30,10, utf8_decode("Sección"), 1, 1, 'c');
 
-    $fpdf->Cell(20, 10, "a", 1, 'c');
     $fpdf->SetFont('Arial','' ,12);
-//finalización del documento
+//creación de los datos 
+    $proyectos = $query->getRankingDESC($idGrado, $idMateria);
+    $c = 1;
+    foreach($proyectos as $camps){
+        $proyectoInfo =$query->getTeambyID($camps['proyecto_idproyecto']);
+        $fpdf->Cell(30,20, $c, 1,0,'c');
+        $fpdf->Cell(85,20,utf8_decode($proyectoInfo['nombreProyecto']), 1, 0,'c');
+        $fpdf->Cell(75, 20, $camps['notafinal'],1,0,'c');
+        $c++;
+        $fpdf->Ln(20);
+    }
+    //finalización del documento
     $fpdf->Output();
 ?>
