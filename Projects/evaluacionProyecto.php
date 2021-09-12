59602a12ff6bdcb2b6c27289a14626d3783baaf4
Diego Mancía <62128556+DiegoSan03@users.noreply.github.com>
2021-09-11 19:37:23 -0600

    Arreglo de bug, evaluación de proyectos
    Bug
    ¿Qué pasa si nadie ha calificado el proyecto?

--- Projects/evaluacionProyecto.php
+++ Projects/evaluacionProyecto.php
@@ -2,7 +2,13 @@
 require('../controlador/pdfEvaluacionesProyectos.php');
 
 if(isset($_GET['proyecto']) && !(empty($_GET['proyecto']))){
-    evaluacionProyecto($_GET['proyecto']);
+    $consulta = new Query; //Crear una consulta
+    $notaFinalProyecto = $consulta->getRankingbyID($_GET['proyecto']); //Promedio final
+    if(empty($notaFinalProyecto)){
+        infoProyecto($_GET['proyecto']);
+    }else{
+        evaluacionProyecto($_GET['proyecto']);
+    }
 }else{
     emptyEvaluacionProyecto();
 }
--- controlador/pdfEvaluacionesProyectos.php
+++ controlador/pdfEvaluacionesProyectos.php
@@ -24,6 +24,76 @@ function emptyEvaluacionProyecto(){
     $pdf->Output("I", $nombre, true);
 }
 
+//Función si no hay datos
+function infoProyecto($idproyecto){
+    //Se crea el PDF
+    $pdf = new FPDF('L');
+    //Se asigna el titulo
+    $pdf->SetTitle("Sistema de Calificación de Crea J", true);
+    $pdf->SetSubject("Sistema de Calificación de Crea J", true);
+    //Se añade una nueva página
+    $pdf->AddPage();
+
+    //-----     Creación de consultas     -----
+    $consulta = new Query; //Crear una consulta
+    $teamData = $consulta->getTeamData($idproyecto); //Obtenemos los datos del proyecto
+    //Generamos un apartado por proyecto
+    foreach($teamData as $campo){
+        //-----     TABLA: PROYECTO     -----
+        //Variables de proyecto
+        $nombreProyecto = $campo['nombreProyecto']; //Nombre
+        $gradoProyecto = $campo['nombre'] ." " . $campo['seccion']; //Grado
+        $materiaProyecto = $campo['nombreMateria']; //Materia
+        $descripcionProyecto = $campo['descripcion']; //Descripción
+        $notaFinalProyecto = $consulta->getRankingbyID($idproyecto); //Promedio final
+        //Dependiendo de que tan largo sea la materia se asignará un espacio espacial para esa fila
+        if(strlen($materiaProyecto) > 21 || strlen($gradoProyecto) > 21){
+            $alturaCelda = 20;
+        }else{
+            $alturaCelda = 10;
+        }
+
+        //Cuerpo de la tabla proyecto
+        $pdf->SetFont('Arial','B',15); //Fuente Negrita
+        $pdf->Cell(150,10,utf8_decode("Proyecto:"), 1, 0, 'L'); //Clave
+        $pdf->Cell(62,10,utf8_decode("Grado:"), 1, 0, 'L'); //Clave
+        $pdf->Cell(65,10,utf8_decode("Materia:"), 1, 1, 'L'); //Clave
+        $pdf->SetFont('Arial','',15); //Fuente Normal
+        $pdf->Cell(150, $alturaCelda,utf8_decode($nombreProyecto), 1, 0, 'L'); //Contenido
+        $pdf->Cell(62, $alturaCelda,utf8_decode($gradoProyecto), 1, 0, 'L'); //Contenido
+        //Dependiendo de que tan largo sea la materia se asignará un espacio espacial para esa fila
+        if($alturaCelda == 20){
+            $pdf->MultiCell(65, 10,utf8_decode($materiaProyecto), 1, 'L'); //Contenido
+        }else{
+            $pdf->Cell(65, $alturaCelda,utf8_decode($materiaProyecto), 1, 1, 'L'); //Contenido
+        }
+        $pdf->SetFont('Arial','B',15); //Fuente Negrita
+        $pdf->Cell(0,10,utf8_decode("Descripción:"), 1, 1, 'L'); //Clave
+        $pdf->SetFont('Arial','',15); //Fuente Normal
+        $pdf->MultiCell(0,10,utf8_decode($descripcionProyecto), 1, 'L'); //Contenido
+        $pdf->SetFont('Arial','B',15); //Fuente Negrita
+        $pdf->Cell(0,10,utf8_decode("Estudiantes:"), 1, 1, 'L'); //Clave
+        $pdf->SetFont('Arial','',15); //Fuente Normal
+
+        //Variables de proyecto
+        $equipoData = $consulta-> obtainTeamMates($idproyecto); //Obtener los nombres de los estudiantes
+        
+        //Estudiantes del proyecto
+        foreach ($equipoData as $key => $estudiante) {
+            $nombreEstudiante = $estudiante['idestudiante']." ".$estudiante['apellidos'];
+            $pdf->Cell(0,10,utf8_decode($nombreEstudiante), "LR", 1, 'L'); //Contenido
+        }
+
+        $pdf->SetFont('Arial','B',15); //Fuente Negrita
+        $pdf->Cell(0,10,utf8_decode("No se ha calificado este proyecto..."), 1, 1, 'L'); //Clave
+    }
+
+    //Obtener nombre del evento para el pdf
+    $nombrePDF = "Evaluacion_Proyecto_" . $nombreProyecto . ".pdf";
+    //Escribir información
+    $pdf->Output("I", $nombrePDF, true);
+}
+
 //Función para imprimir pdf con la evaluacion de un proyecto
 function evaluacionProyecto($idproyecto){
     //-----     CREACIÓN DEL PDF     -----
