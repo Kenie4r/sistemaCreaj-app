135cb18e6a3c3d9d793b1f823b10efe1f5cd6cb2
MatMT <oscarmateoelias@gmail.com>
2021-09-16 15:53:06 -0600

    Buton 2 js

--- Dashboard/profile.php
+++ Dashboard/profile.php
@@ -7,7 +7,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <!--<script src="js/dashboard.js"></script> -->
     <link rel="stylesheet" href="../recursos/icons/style.css">
-   
+    <script src="button.js"></script>
+    <script src="js/button2.js"></script>
 </head>
 <body >
 <?php
--- academico/index.php
+++ academico/index.php
@@ -16,6 +16,7 @@ require_once("../modelo/query.php");
     <link rel="stylesheet" href="../recursos/icons/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="../Dashboard/button.js"></script>
+    <script src="../Dashboard/js/button2.js"></script>
 </head>
 <body>
 <?php
