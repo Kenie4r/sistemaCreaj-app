9ef66ae1cefb2e6f3e5e0cdebcb535c99e7b7889
Kenie4r <54483710+Kenie4r@users.noreply.github.com>
2021-09-17 08:03:54 -0600

    Update getprojectsinRate.php

--- controlador/getprojectsinRate.php
+++ controlador/getprojectsinRate.php
@@ -76,66 +76,85 @@ require_once("../modelo/query.php");
                 }
             }
         }else{
-      
-            foreach($info as $result){
-                $calificado = $query->isSavedProject($result['idproyecto'], $userID);
-                foreach( $calificado as $camp){
-                    foreach($camp as $myre){
-                        if($myre>0){
-                            $c+=1;
-                            $graded = $query->getPoints($userID,$result['idproyecto'] );
-                            foreach($graded as $mycamp){
-                                $grade = $mycamp['points'];
-                            }
-                            $withGrade .="<div class='bg-white sm:w-8/12 w-10/12 border border-2 p-5 roundend-full m-auto shadow-md my-7'>
-                            <div class='flex w-full flex-col sm:flex-row items-center justify-between'>
-                                    <div class='text-lg font-bold '>
-                                        <h1>{$result['nombreProyecto'] }</h1>
+            if(!empty($info)){
+                foreach($info as $result){
+                    $calificado = $query->isSavedProject($result['idproyecto'], $userID);
+                    foreach( $calificado as $camp){
+                        foreach($camp as $myre){
+                            if($myre>0){
+                                $c+=1;
+                                $graded = $query->getPoints($userID,$result['idproyecto'] );
+                                foreach($graded as $mycamp){
+                                    $grade = $mycamp['points'];
+                                }
+                                $withGrade .="<div class='bg-white sm:w-8/12 w-10/12 border border-2 p-5 roundend-full m-auto shadow-md my-7'>
+                                <div class='flex w-full flex-col sm:flex-row items-center justify-between'>
+                                        <div class='text-lg font-bold '>
+                                            <h1>{$result['nombreProyecto'] }</h1>
+                                        </div>
+                                        <div class='text-center '>
+                                            <p class='p-1 border border-2 border-blue-400 text-blue-400 w-40 mx-auto mt-2'>
+                                               Obtuvo: {$grade}
+                                            </p>
+                                        </div>
                                     </div>
-                                    <div class='text-center '>
-                                        <p class='p-1 border border-2 border-blue-400 text-blue-400 w-40 mx-auto mt-2'>
-                                           Obtuvo: {$grade}
-                                        </p>
+                                    <div>
+                                        {$result['descripcion']}
+                                    </div>   
+                                    <div class='w-full flex flex-row items-center text-sm text-white my-1'> 
+                                        <div class='text-gray-400 p-1 rounded-lg m-1'><span class='icon-book'></span> {$dataMateria['nombre']}</div>
+                                        <div class='text-gray-400 p-1 rounded-lg m-1'><span class='icon-user'></span> {$dataGrado[0]['nombre']} {$dataGrado[0]['seccion']}</div>
                                     </div>
                                 </div>
-                                <div>
-                                    {$result['descripcion']}
-                                </div>   
-                                <div class='w-full flex flex-row items-center text-sm text-white my-1'> 
-                                    <div class='text-gray-400 p-1 rounded-lg m-1'><span class='icon-book'></span> {$dataMateria['nombre']}</div>
-                                    <div class='text-gray-400 p-1 rounded-lg m-1'><span class='icon-user'></span> {$dataGrado[0]['nombre']} {$dataGrado[0]['seccion']}</div>
-                                </div>
-                            </div>
-                        ";
-                        }else{ 
-                        
-                            $html.="<div class='bg-white sm:w-8/12 w-10/12 border border-2 p-5 roundend-full m-auto shadow-md my-7'>
-                                                <div class='flex w-full flex-col sm:flex-row items-center justify-between'>
-                                                        <div class='text-lg font-bold '>
-                                                            <h1>{$result['nombreProyecto'] }</h1>
+                            ";
+                            }else{ 
+                            
+                                $html.="<div class='bg-white sm:w-8/12 w-10/12 border border-2 p-5 roundend-full m-auto shadow-md my-7'>
+                                                    <div class='flex w-full flex-col sm:flex-row items-center justify-between'>
+                                                            <div class='text-lg font-bold '>
+                                                                <h1>{$result['nombreProyecto'] }</h1>
+                                                            </div>
+                                                            <div class='text-center '>
+                                                                <p class='p-1 border border-2 border-blue-400 text-blue-400 w-40 mx-auto mt-2 hover:bg-blue-400 hover:text-white  cursor-pointer'>
+                                                                    <a href='calificar.php?teamID={$result['idproyecto']}'>
+                                                                        Calificar proyecto
+                                                                    </a>
+                                                                </p>
+                                                            </div>
                                                         </div>
-                                                        <div class='text-center '>
-                                                            <p class='p-1 border border-2 border-blue-400 text-blue-400 w-40 mx-auto mt-2 hover:bg-blue-400 hover:text-white  cursor-pointer'>
-                                                                <a href='calificar.php?teamID={$result['idproyecto']}'>
-                                                                    Calificar proyecto
-                                                                </a>
-                                                            </p>
-                                                        </div>
+                                                        <div>
+                                                            {$result['descripcion']}
+                                                        </div>   
+                                                        <div class='w-full flex flex-row items-center text-sm text-white my-1'> 
+                                        <div class='text-gray-400 p-1 rounded-lg m-1'><span class='icon-book'></span> {$dataMateria['nombre']}</div>
+                                        <div class='text-gray-400 p-1 rounded-lg m-1'><span class='icon-user'></span> {$dataGrado[0]['nombre']} {$dataGrado[0]['seccion']}</div>
+                                    </div>
                                                     </div>
-                                                    <div>
-                                                        {$result['descripcion']}
-                                                    </div>   
-                                                    <div class='w-full flex flex-row items-center text-sm text-white my-1'> 
-                                    <div class='text-gray-400 p-1 rounded-lg m-1'><span class='icon-book'></span> {$dataMateria['nombre']}</div>
-                                    <div class='text-gray-400 p-1 rounded-lg m-1'><span class='icon-user'></span> {$dataGrado[0]['nombre']} {$dataGrado[0]['seccion']}</div>
-                                </div>
-                                                </div>
-                                    ";
-                        
+                                        ";
+                            
+                            }
                         }
                     }
                 }
+
+            }else{
+                $html = "
+                <div class='text-gray-400 text-xl w-8/12 h-full m-auto  mt-10'>
+                    <h1 class='text-center text-6xl'>
+                    <span class='icon-sad2'></span>
+                </h1>
+                <h1 class='text-center'>
+                    No se encuentra ningún proyecto.
+                </h1>
+                 </div>
+                
+                
+                ";
+
+
+
             }
+        
         }
     
     
