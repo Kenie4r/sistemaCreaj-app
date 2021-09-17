5c6310b27335d2e6bd62b77315fd7b6c06666a91
jesusmejia04 <79819116+jesusmejia04@users.noreply.github.com>
2021-09-16 20:53:41 -0600

    profile

--- Dashboard/profile.php
+++ Dashboard/profile.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crea J | Inicio</title>
     <link rel="stylesheet" href="css/style.css">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
