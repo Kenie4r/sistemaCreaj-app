432f1f92f0ce1d9620c6fde415a55c412b9d1b4a
Fernando Lemus <67608658+Pollalta@users.noreply.github.com>
2021-11-02 10:19:10 -0600

    numeros en grados
    asaa

--- grades/js/GradesComparison.js
+++ grades/js/GradesComparison.js
@@ -8,11 +8,12 @@ function ver() {
     //ReGrado=false
     //ReSeccion=false
     //ReNivel=false
+    Innum=/\d/g;
     niveles=document.querySelector('#nivel').value
     grado=document.querySelector('#grado').value
     seccion=document.querySelector('#seccion').value
     
-    if (niveles==""||niveles=="Selecciona") {
+    if (niveles==""||niveles=="Selecciona"||niveles=="No hay niveles") {
        document.querySelector("#te").innerHTML="Escoja una opcion viable"; 
        $("#te").css("color","red");
         ReNivel=false
@@ -23,6 +24,19 @@ function ver() {
     if (grado!="") {
         ReGrado=true
         $("#gra").empty();
+        /*if (Innum.exec(grado)>Inletra.exec(grado)) {
+            document.querySelector("#te").innerHTML="Formato de nombre no permitido"; 
+            $("#te").css("color","red");
+             ReNivel=false
+        }
+        console.log(Innum.exec(grado))
+        console.log(Inletra.exec(grado))*/
+        if (!isNaN(grado) ) {
+            document.querySelector("#gra").innerHTML="Formato de grado no permitido"; 
+            $("#gra").css("color","red");
+             ReNivel=false
+        }
+        
     }else{
         ReGrado=false
         document.querySelector("#gra").innerHTML="Escribe el Nombre del grado"; 
@@ -31,6 +45,11 @@ function ver() {
     if (seccion!="") {
         ReSeccion=true
         $("#sec").empty();
+        if (!isNaN(seccion)) {
+            ReSeccion=false
+        document.querySelector("#sec").innerHTML="No se permite números"; 
+        $("#sec").css("color","red");
+        }
     }else{
         ReSeccion=false
         document.querySelector("#sec").innerHTML="Escribe el indice de la seccion"; 
